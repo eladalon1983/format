d48d3b6a308e535bc5216496c9da2d23be047242
Elad Alon <eladalon@google.com>
2024-09-17 15:40:36 +0200

    bla bla bla

---
+++ element-capture.js
@@ -0,0 +1,28 @@
+var respecConfig = {
+  group: "cg/sccg",
+  specStatus: "CG-DRAFT",
+  github: {
+    repoURL: "https://github.com/screen-share/element-capture/",
+    branch: "main",
+  },
+  editors: [
+    {
+      name: "Elad Alon",
+      email: "eladalon@google.com",
+      company: "Google",
+      w3cid: 118124,
+    },
+  ],
+  xref: [
+    "html",
+    "infra",
+    "permissions",
+    "permissions-policy",
+    "dom",
+    "mediacapture-streams",
+    "mediacapture-region",
+    "webidl",
+    "screen-capture",
+  ],
+  subjectPrefix: "[element-capture]",
+};
